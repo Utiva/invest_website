1d0354ecc5d15e9f93a3ffefe602533a1c3e179d
Abayomi Olatunji <abayomijohn273@gmail.com>
2026-06-19 15:12:11 +0100

    Update Tailwind CSS configuration to include new font families and sizes, and add custom font-face definitions in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Matter", ...defaultTheme.fontFamily.sans],
+        mono: ["TT Firs Neue", ...defaultTheme.fontFamily.mono],
+      },
+      fontSize: {
+        "sm-15": "0.9375rem", //15px
+      },
       colors: {
         primary: "#050794",
         secondary: "#FBAF1B",
         "primary-dark": "#000269",
         "deep-blue": "#0C0E83",
-      },
-      fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
       },
     },
   },
