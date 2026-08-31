810550d17c3a66fe8427bd7031369f72ce480740
Joseph Ijaola <ijabolu@gmail.com>
2026-08-31 15:22:29 +0100

    update: change the youtube link

--- src/pages/DemoPage.jsx
+++ src/pages/DemoPage.jsx
@@ -25,7 +25,7 @@ export default function DemoPage() {
           <div className="rounded-2xl overflow-hidden shadow-2xl" style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
             <iframe
               style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: 0 }}
-              src="https://www.youtube.com/embed/dSugXydAK7o?si=k0eqQTjlKi7y5VGz"
+              src="https://www.youtube.com/embed/wxkfgFqv6_c?si=DSR-TZL8bnNTxwZ5"
               title="Utiva Platform Walkthrough for Employers and Talent"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
